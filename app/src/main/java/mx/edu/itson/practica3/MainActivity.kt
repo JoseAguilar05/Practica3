c00baca4917db1fb6ff510fb4f5b77108b9f5b36
José Eduardo Aguilar García <jose.aguilar252049@potros.itson.edu.mx>
2026-02-16 20:21:23 -0700

    prueba commit v2

--- app/src/main/java/mx/edu/itson/practica3/MainActivity.kt
+++ app/src/main/java/mx/edu/itson/practica3/MainActivity.kt
@@ -7,7 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-// Jose Eduardo Aguilar García IDDDD:00000252049
+// Jose Eduardo Aguilar García 00000252049
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
