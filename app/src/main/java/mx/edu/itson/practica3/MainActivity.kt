cff6bbabcb321539ad8497d7ace9d12b8da1cb42
José Eduardo Aguilar García <jose.aguilar252049@potros.itson.edu.mx>
2026-02-16 20:05:12 -0700

    commit prueba

---
+++ app/src/main/java/mx/edu/itson/practica3/MainActivity.kt
@@ -0,0 +1,34 @@
+package mx.edu.itson.practica3
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+// Jose Eduardo Aguilar García ID:00000252049
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+
+
+        val button: Button = findViewById(R.id.btn_get_started)
+
+
+        button.setOnClickListener {
+            var intent: Intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
