307565c42739ad4837e1cee025a61d04f4a4363a
José Eduardo Aguilar García <jose.aguilar252049@potros.itson.edu.mx>
2026-02-18 12:32:37 -0700

    Practica 4
    Se actualizan textos del menú a categorías: Antojitos, Especialidades, etc.

---
+++ app/src/main/java/mx/edu/itson/practica3/Product.kt
@@ -0,0 +1,9 @@
+package mx.edu.itson.practica3
+
+data class Product (
+    // Jose Eduardo Aguilar García 00000252049
+    var name: String,
+    var image: Int,
+    var description: String,
+    var price: Double
+)
